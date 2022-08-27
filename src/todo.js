9501b053eed9aca5dc0795030afa95594aa7be62
AndrewKim330 <rendezvous330@gmail.com>
2022-08-27 15:25:49 +0900

    feat: Construct basic structure of TODO application(class)

---
+++ src/todo.js
@@ -0,0 +1,28 @@
+class Todo {
+    // @ts-check
+    
+    /**
+     * Add TODO instance
+     */
+    constructor() 
+
+    /**
+     * Create single TODO element
+     */
+    createSingleTodo ()
+
+    /**
+     * Read TODO element(s)
+     */
+    readTodo ()
+
+    /**
+     * Update single TODO element
+     */
+    updateSingleTodo ()
+
+    /**
+     * Delete TODO element(s)
+     */
+    deleteTodo ()
+}
